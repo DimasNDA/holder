571b60932be0e45f6467e0d522a28158a1047f36
Nikitchuk Dmytro <90049427+DimasNDA@users.noreply.github.com>
2023-12-06 17:25:58 +0200

    finish project
    finish project

---
+++ constants/catalog.js
@@ -0,0 +1,56 @@
+const CATALOG = [
+    {
+        id: 'p1',
+        name: 'Xiaomi MI QI Car',
+        img: 'img/catalog/p1.png',
+        price: 1599,
+    },
+    {
+        id: 'p2',
+        name: 'ColorWay Gravity Sensor Holder',
+        img: 'img/catalog/p2.png',
+        price: 299,
+    },
+    {
+        id: 'p3',
+        name: 'СolorWay Dashboard-1',
+        img: 'img/catalog/p3.png',
+        price: 599,
+    },
+    {
+        id: 'p4',
+        name: 'ColorWay Car Wireless Charger 2',
+        img: 'img/catalog/p4.png',
+        price: 999,
+    },
+    {
+        id: 'p5',
+        name: 'Baseus Gravity Car Mount',
+        img: 'img/catalog/p5.png',
+        price: 759,
+    },
+    {
+        id: 'p6',
+        name: 'Intelligent Wireless Charger',
+        img: 'img/catalog/p6.png',
+        price: 1199,
+    },
+    {
+        id: 'p7',
+        name: 'Nomi CM-01',
+        img: 'img/catalog/p7.png',
+        price: 99,
+    },
+    {
+        id: 'p8',
+        name: 'Baseus Big Mouth Pro Car Mount',
+        img: 'img/catalog/p8.png',
+        price: 399,
+    },
+    {
+        id: 'p9',
+        name: 'HOCO Center console magnetic',
+        img: 'img/catalog/p9.png',
+        price: 699,
+    },
+];
